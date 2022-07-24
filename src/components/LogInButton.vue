2fb53233bff707511a59f5d3bc7d0bb6a8d40d2d
JarodYli <jarodylie@gmail.com>
2022-07-24 17:42:47 -0400

    styled log in button, rounded and green

--- src/components/LogInButton.vue
+++ src/components/LogInButton.vue
@@ -3,9 +3,23 @@
 </script>
 
 <template>
-    <button>Log In</button>
+    <button id="login-button">Log In</button>
 </template>
 
 <style scoped>
+
+#login-button
+{
+    background-color: rgb(27,169,76);
+    color: white;
+    border-radius: 1000px;
+    border: 0ch;
+    font-size: 20px;
+    box-shadow: 1px 1px 5px black;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 40px;
+    padding-right: 40px;
+}
 
 </style>
